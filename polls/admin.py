cd22ef52444f93333ebf4d5cfa8b86872395395e
Douglas Matoso <doug2k1@gmail.com>
2011-08-30 21:25:22 -0300

    Views and admin customization

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,19 @@
-from polls.models import Poll
+from polls.models import Poll, Choice
 from django.contrib import admin
 
-admin.site.register(Poll)
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 3
+
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,					{'fields': ['question']}),
+        ('Date information',	{'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question', 'pub_date', 'was_published_today')
+    list_filter = ['pub_date']
+    search_fields = ['question']
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice)
